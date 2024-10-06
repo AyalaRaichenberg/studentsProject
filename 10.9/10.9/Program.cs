6434c4261468ef95c7c616779bbfee18a6420dd8
AyalaRaichenberg <ayalar8648@gmail.com>
2024-10-06 13:06:44 +0300

    Add files via upload

---
+++ 10.9/10.9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._9
+{
+    internal class Program
+    {
+
+        static void printStudentLinst(List<Student> students)
+        {
+            for(int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine("hello "+ students[i].getName()+", you learn "+ students[i].getSubject());
+                Console.WriteLine("your courses: ");
+                students[i].showCourse();
+                students[i].showScores();
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            //סעיף 3
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hello student number "+(1+i));
+                Console.WriteLine("Enter yuor name and your subject: ");
+                Student s = new Student(Console.ReadLine(), Console.ReadLine());
+                s.addCourse();
+                Console.WriteLine("Enter the scores of the last 2 tests:");
+                for (int j = 0; j < 2; j++) {
+                    s.addTest(Console.ReadLine(), Int32.Parse(Console.ReadLine()));
+                }
+                students.Add(s);
+            }
+            printStudentLinst(students);
+
+            // סעיף 4
+            List<Student> nameStartWith = students.Where(s => s.getName().StartsWith("a")).ToList();
+            List<Student> subjectStartWith = students.Where(s => s.getSubject().Equals("teaching")).ToList();
+            List<Student> manyCuorses = students.Where(s => s.getAmountOfCourses()>1).ToList();
+            List<Student> lowMark = students.Where(s => s.mark56()).ToList();
+
+            Console.WriteLine("Students whose name begins with the letter a:");
+            printStudentLinst(nameStartWith);
+            Console.WriteLine("Students whose profession is teaching:");
+            printStudentLinst(subjectStartWith);
+            Console.WriteLine("Students whose number of courses is greater than 1:");
+            printStudentLinst(manyCuorses);
+            Console.WriteLine("Students who have a score lower than 56:");
+            printStudentLinst(lowMark);
+
+        }
+        
+    }
+}
---
+++ 10.9/10.9/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._9
+{
+    internal class Student
+    {
+        private String name;
+        private String subject;
+        private List<String> courses;
+        private Dictionary<String, int> tests;
+
+        public Student(String name, String subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            courses = new List<string>();
+            tests = new Dictionary<String, int>();
+        }
+        public String getName() {  return name; }
+        public String getSubject() { return subject; }
+        public int getAmountOfCourses() { return courses.Count; }
+        public Boolean mark56()
+        {
+            foreach (var item in tests.Values)
+            {
+                if (item < 56)
+                    return true;
+            }
+            return false;
+        }
+
+        //סעיף 1
+        public void showCourse()
+        {
+            foreach (String item in courses)
+            {
+                Console.WriteLine(item+" "); 
+            }
+        }
+
+        
+        public void addCourse()
+        {
+            Console.WriteLine(name+" enter the name of the course");
+            String input =    Console.ReadLine();
+            while (!input.Equals("0"))
+            {
+                courses.Add(input);
+                Console.WriteLine("Enter the name of the course");
+                input = Console.ReadLine();
+                
+            }
+          
+            Console.WriteLine("The courses were successfully taken");
+            
+        }   
+        
+
+        //סעיף 2
+        public void addTest(String subject,int mark)
+        {
+            tests.Add(subject,mark);
+        }
+
+        public void showScores()
+        {
+            Console.WriteLine("hellow "+name+" your marks in your tests: ");
+            foreach (var test in tests)
+            {
+                Console.WriteLine("subject :" +test.Key+" ,your mark: "+ test.Value);
+            }
+        }
+
+    }
+}
